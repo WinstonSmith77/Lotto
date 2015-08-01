45955142bf7dfa8d51d000b1469ffbf49b499094
WinstonSmith77 <matze_henning@gmx.de>
2015-08-01 17:49:22 +0200

    Es geht los

---
+++ Lotto/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lotto
+{
+    class Program
+    {
+        static void Main()
+        {
+            const int numberOfBalls = 49;
+            const int choosen = 6;
+            var balls = Enumerable.Range(1, numberOfBalls).ToList();
+            var random = new Random();
+
+            var result = new List<int>();
+
+            for (int i = 0; i <= choosen; i++)
+            {
+                var nextPick = random.Next(balls.Count - 1);
+                result.Add(balls[nextPick]);
+                balls.RemoveAt(nextPick);
+            }
+
+
+
+            result = result.OrderBy(ball => ball).ToList();
+
+
+            result.ForEach(Console.WriteLine);
+            Console.ReadKey();
+        }
+    }
+}
